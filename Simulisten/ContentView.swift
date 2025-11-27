e5bb30eac427d72670392cdb35ee58e8e295df23
Brendan Rodriguez <brendantr57@aol.com>
2025-11-27 11:13:12 -0500

    added the rest of the Big Book audio files Chapters 1 - 11.

--- Simulisten/ContentView.swift
+++ Simulisten/ContentView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct ContentView: View {
     @StateObject private var audioManager = AudioManager()
 
-    let bookOptions = ["book1.mp3", "book2.mp3", "book3.mp3", "book4.mp3", "book5.mp3", "book6.mp3", "book7.mp3", "book8.mp3"]
+    let bookOptions = ["book1.mp3", "book2.mp3", "book3.mp3", "book4.mp3", "book5.mp3", "book6.mp3", "book7.mp3", "book8.mp3", "book9.mp3",         "book10.mp3", "book11.mp3", "book12.mp3", "book13.mp3", "book14.mp3", "book15.mp3", "book16.mp3", "book17", "book18.mp3", "book19.mp3"]
     let musicOptions = ["music1.mp3", "music2.mp3", "music3.mp3", "music4.mp3", "music5.mp3", "music6.mp3", "music7.mp3", "music8.mp3"]
 
     @State private var selectedBook = "book1.mp3"
