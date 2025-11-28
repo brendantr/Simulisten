c3225c377e7882febb7e55dab4581ae36a8849d3
Brendan Rodriguez <brendantr57@aol.com>
2025-11-28 09:16:04 -0500

    added music 9 - 12 mp3 files

--- Simulisten/ContentView.swift
+++ Simulisten/ContentView.swift
@@ -4,7 +4,7 @@ struct ContentView: View {
     @StateObject private var audioManager = AudioManager()
 
     let bookOptions = ["book1.mp3", "book2.mp3", "book3.mp3", "book4.mp3", "book5.mp3", "book6.mp3", "book7.mp3", "book8.mp3", "book9.mp3",         "book10.mp3", "book11.mp3", "book12.mp3", "book13.mp3", "book14.mp3", "book15.mp3", "book16.mp3", "book17", "book18.mp3", "book19.mp3"]
-    let musicOptions = ["music1.mp3", "music2.mp3", "music3.mp3", "music4.mp3", "music5.mp3", "music6.mp3", "music7.mp3", "music8.mp3"]
+    let musicOptions = ["music1.mp3", "music2.mp3", "music3.mp3", "music4.mp3", "music5.mp3", "music6.mp3", "music7.mp3", "music8.mp3", "music9.mp3", "music10.mp3", "music11.mp3", "music12.mp3"]
 
     @State private var selectedBook = "book1.mp3"
     @State private var selectedMusic = "music1.mp3"
